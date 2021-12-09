3b914f7d467835ee1f30b0ea6840781339168c1a
sergioqueber <91956609+sergioqueber@users.noreply.github.com>
2021-12-09 16:52:36 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 
+let sonar = 0
+radio.setGroup(1)
   basic.forever(function () {
   
   sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+  radio.onReceivedNumber(function (receivedNumber: number){
+      if (receivedNumber == 7) {
+        cuteBot.stopcar()
+      } else {
   
   if (sonar > 5 && sonar < 10) {
   cuteBot.stopcar()
@@ -10,24 +16,23 @@
      basic.pause(1000)  
      music.playTone(880, music.beat(BeatFraction.Quarter))
     }
+    basic.pause(1000)
+     if (sonar > 5 && sonar < 10) {
     cuteBot.moveTime(cuteBot.Direction.backward, 35, 0.3)
     basic.pause(2000)
     cuteBot.moveTime(cuteBot.Direction.right, 30, 0.4)
     basic.pause(2000)
-    cuteBot.moveTime(cuteBot.Direction.forward, 30, 1)
+    cuteBot.moveTime(cuteBot.Direction.forward, 30, 0.5)
     cuteBot.moveTime(cuteBot.Direction.left, 30, 0.4)
-    
+     }
+    else {
+     cuteBot.moveTime(cuteBot.Direction.forward, 30, 0.5)
+     
+    }
   } else {
   
-      if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-          cuteBot.motors(40, 15)
-      }
-      if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-          cuteBot.motors(15, 40)
-      }
-      if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
-          cuteBot.motors(40, 40)
-      }
+    follow_line()
+   
   }
      /* if (sonar > 5 && sonar < 10) {
           cuteBot.stopcar()
@@ -43,9 +48,11 @@
           cuteBot.moveTime(cuteBot.Direction.forward, 30, 1)
           cuteBot.moveTime(cuteBot.Direction.right, 30, 0.4)
       }*/
+      }
+    })
   })
    
-let sonar = 0
+
  /*basic.forever(function () {
  
   sonar = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
@@ -64,4 +71,19 @@ let sonar = 0
   }
   })
 */
-
+function stop_if_line (){
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.stopcar()
+    }
+}
+function follow_line() {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(40, 15)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(15, 40)
+    }
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.motors(40, 40)
+    }
+}
